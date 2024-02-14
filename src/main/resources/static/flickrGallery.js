0f9057787a755ffe0be13547b685cbdee6efdfe0
Julia <julrok-2@student.ltu.se>
2024-02-14 16:07:43 +0100

    FungerandeFrontendDelvis

--- src/main/resources/static/flickrGallery.js
+++ src/main/resources/static/flickrGallery.js
@@ -1,9 +1,34 @@
-// API Base URL - serveradressen
 const API_BASE_URL = "http://localhost:3000";
+const PAGE_SIZE = 5;
+let currentPage = 1;
+let isLoading = false;
 
-async function fetchImages() {
+document.getElementById("search-button").addEventListener("click", () => {
     const textInput = document.getElementById("text-input").value;
-    const url = `${API_BASE_URL}/json?text=${encodeURIComponent(textInput)}`;
+    if (!textInput) {
+        alert("Vänligen skriv in text");
+        return;
+    }
+    currentPage = 1;
+    fetchImages(textInput).then(r => console.log(r) );
+});
+
+window.addEventListener("scroll", () => {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+        if (!isLoading) {
+            isLoading = true;
+            currentPage++;
+            const textInput = document.getElementById("text-input").value;
+            fetchImages(textInput, currentPage).then(r => console.log(r));
+        }
+    }
+});
+
+async function fetchImages(textInput, page) {
+    const url = `${API_BASE_URL}/json?text=${encodeURIComponent(textInput)}&per_page=${PAGE_SIZE}&page=${page}`;
+
+
+    showLoading(true);
 
     console.log(url);
     if (!textInput) {
@@ -16,27 +41,41 @@ async function fetchImages() {
         console.log(response);
         if (response.ok) {
             const data = await response.json();
-            jsonFlickrApi(data);
+            displayImages(data);
         } else {
             alert("Misslyckades att hämta data från servern");
             console.error(`Failed to fetch data. Status: ${response.status}`);
         }
     } catch (error) {
         console.error('Error:', error);
+        showError(true);
     }
+    showLoading(false);
 }
 
-function jsonFlickrApi(rsp){
+function displayImages(rsp){
     if (rsp.stat != "ok"){
         // something broke!
         console.error(rsp.message);
         return;
     }
 
+    const imagesContainer = document.getElementById("images-container");
+    imagesContainer.innerHTML = "";
     for (var i=0; i<rsp.photos.photo.length; i++){
         var photo = rsp.photos.photo[i];
         var img = document.createElement('img');
         img.src = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-        document.body.appendChild(img);
+        imagesContainer.appendChild(img);
     }
+}
+
+function showLoading(isLoading) {
+    const loading = document.getElementById("loading");
+    loading.classList.toggle("hidden", !isLoading);
+}
+
+function showError(hasError) {
+    const error = document.getElementById("error");
+    error.classList.toggle("hidden", !hasError);
 }
